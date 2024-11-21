b4e65ae8124ab0b1b9cd7e58262d24752727a10d
Troy Washington <troyvw96@gmail.com>
2024-11-20 17:44:43 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby App`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
